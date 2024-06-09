d7fe0920c4cb16e0f73050b2340b4cbe83209cb9
Matthias <matthias@endler.dev>
2024-06-09 16:33:24 +0200

    Enable all fibonacci tests

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -22,16 +22,16 @@ mod tests {
 
     #[test]
     fn test_fibonacci() {
-        // assert_eq!(fibonacci(0), 0);
-        // assert_eq!(fibonacci(1), 1);
-        // assert_eq!(fibonacci(2), 1);
+        assert_eq!(fibonacci(0), 0);
+        assert_eq!(fibonacci(1), 1);
+        assert_eq!(fibonacci(2), 1);
         assert_eq!(fibonacci(3), 2);
-        // assert_eq!(fibonacci(4), 3);
-        // assert_eq!(fibonacci(5), 5);
-        // assert_eq!(fibonacci(6), 8);
-        // assert_eq!(fibonacci(7), 13);
-        // assert_eq!(fibonacci(8), 21);
-        // assert_eq!(fibonacci(9), 34);
-        // assert_eq!(fibonacci(10), 55);
+        assert_eq!(fibonacci(4), 3);
+        assert_eq!(fibonacci(5), 5);
+        assert_eq!(fibonacci(6), 8);
+        assert_eq!(fibonacci(7), 13);
+        assert_eq!(fibonacci(8), 21);
+        assert_eq!(fibonacci(9), 34);
+        assert_eq!(fibonacci(10), 55);
     }
 }
